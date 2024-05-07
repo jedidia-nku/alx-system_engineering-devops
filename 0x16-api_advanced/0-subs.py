e46ebc03bef36cf85d221a129f6ea0a0cce6bc75
jedidia-nku <jedinkuze123@gmail.com>
2024-05-07 06:05:46 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"subscribers count"
-
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-    else:
-        return data['data']['subscribers']
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
